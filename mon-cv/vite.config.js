9a8cf4f1a1af79c9f476f150db67f3e2d1311668
IUT BLAGNAC <noreply@univ-tlse2.fr>
2025-03-10 16:25:15 +0100

    Update vite.config.js

--- mon-cv/vite.config.js
+++ mon-cv/vite.config.js
@@ -3,13 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/mon-cv/',
-  optimizeDeps: {
-    include: ['vue']  // 🟣 Forcer l'inclusion de Vue dans le bundle
-  },
-  build: {
-    commonjsOptions: {
-      include: [/node_modules/],  // 🟣 Gérer les imports CommonJS
-    }
-  }
+  base: '/'  // 🟣 Mettre juste '/' car le site est à la racine !
 })
